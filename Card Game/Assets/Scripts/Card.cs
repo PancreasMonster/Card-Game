dbca3db981b176c0fee3def3e869d7875eaae5bd
Tobias Hallen <32342223+TobiasHallen@users.noreply.github.com>
2019-08-29 16:42:12 +0100

    Adding Skeleton Classes

---
+++ Card Game/Assets/Scripts/Card.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public string Name;
+    public int Cost;
+    public string Type;
+
+    public string Ability;
+    public int attack;
+    public int defense;
+
+    public string cardText;
+    public string flavorText;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
