35932942df49954cc88fe4e776470cb40669cc21
Patnjop <36369847+Patnjop@users.noreply.github.com>
2019-08-29 15:11:58 +0100

    Scripts I added

---
+++ Card Game/Assets/Scripts/HandManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandManager : MonoBehaviour
+{
+
+    public int handSize;
+    public int currenthandSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        handSize = 4;
+        DrawHand();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DrawHand()
+    {
+        if (currenthandSize < handSize)
+        {
+            currenthandSize++;
+        }
+    }
+}
