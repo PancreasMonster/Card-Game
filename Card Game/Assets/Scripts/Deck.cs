f6080f8f6f4648183be58edd35c6b746e1461763
Patnjop <36369847+Patnjop@users.noreply.github.com>
2019-08-29 17:07:26 +0100

    Cards Instantiated

--- Card Game/Assets/Scripts/Deck.cs
+++ Card Game/Assets/Scripts/Deck.cs
@@ -6,16 +6,29 @@ public class Deck : MonoBehaviour
 {
 
     public int numberOfCards;
-    public LinkedList<Card> cards;
+    public List<GameObject> cards;
+    int startingDeckSize = 10;
+    int handSize = 5;
 
     void Start()
     {
-        
+        numberOfCards = startingDeckSize;
+        DrawHand();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void DrawHand()
+    {
+        for (int c = 0; c < handSize; c++)
+        {
+            GameObject firstCard = Instantiate(cards[0], this.transform.position, Quaternion.identity).gameObject;
+            cards.Remove(cards[0]);
+            numberOfCards--;
+        }
     }
 }
