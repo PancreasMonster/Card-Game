9dd03821d2dc58fbe5a9f61e3a202d2f323b0081
Patnjop <36369847+Patnjop@users.noreply.github.com>
2019-08-29 18:34:28 +0100

    Scripts n Stuff

---
+++ Card Game/Assets/Scripts/DiscardPile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscardPile : MonoBehaviour
+{
+    public List<GameObject> discardPile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Card Game/Assets/Scripts/Hand.cs
+++ Card Game/Assets/Scripts/Hand.cs
@@ -6,7 +6,7 @@ public class Hand : MonoBehaviour
 {
     public int numberOfCards;
     public List<GameObject> cards;
-
+    bool endRound = false;
 
     void Start()
     {
@@ -16,6 +16,23 @@ public class Hand : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        numberOfCards = cards.Count;
+        if (Input.GetKeyDown(KeyCode.R) && endRound == false)
+        {
+            RoundEnd();
+            endRound = true;
+        }
+    }
+
+    public DiscardPile discardPile;
+    void RoundEnd()
+    {
+        for (int h = 0; h < numberOfCards; h++)
+        {
+            discardPile.discardPile.Add(cards[0]);
+            DestroyImmediate(cards[0], true);
+            cards.Remove(cards[0]);
+        }
+        endRound = false;
     }
 }
