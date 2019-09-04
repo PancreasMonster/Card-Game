19ec4a389668b5b2146f37d83208704916c0f047
Patnjop <36369847+Patnjop@users.noreply.github.com>
2019-09-04 03:54:53 +0100

    Scriptable objects for cards and project folder adjusting

---
+++ Card Game/Assets/Scripts/Card.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Unit", menuName = "Cards/Unit")]
+
+public class Unit : ScriptableObject
+{
+    public new string name;
+    public string description;
+
+    public Sprite artwork;
+
+    public int Cost;
+    public int attack;
+    public int defense;
+}
+
+[CreateAssetMenu(fileName = "New Action", menuName = "Cards/Action")]
+public class Action : ScriptableObject
+{
+    public new string name;
+    public string description;
+
+    public Sprite artwork;
+
+    public int Cost;
+}
+
+[CreateAssetMenu(fileName = "New Project", menuName = "Cards/Project")]
+public class Project : ScriptableObject
+{
+    public new string name;
+    public string objective;
+    public string reward;
+
+    public Sprite artwork;
+
+    public int Cost;
+}
---
+++ Card Game/Assets/Scripts/UnitDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitDisplay : MonoBehaviour
+{
+
+    public Unit unit;
+
+    public Text nameText;
+    public Text descriptionText;
+
+    public Image artworkImage;
+
+    public Text costText;
+    public Text attackText;
+    public Text defenseText;
+
+
+    void Start()
+    {
+        nameText.text = unit.name;
+        descriptionText.text = unit.description;
+
+        artworkImage.sprite = unit.artwork;
+
+        costText.text = unit.Cost.ToString();
+        attackText.text = unit.attack.ToString();
+        defenseText.text = unit.defense.ToString();
+    }
+
+}
